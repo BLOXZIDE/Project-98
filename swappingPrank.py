5307d02c777de3a2842d071881ea7a271e21c1d5
BLOXZIDE <82806724+BLOXZIDE@users.noreply.github.com>
2022-06-18 13:31:00 +0530

    Add files via upload

---
+++ swappingPrank.py
@@ -0,0 +1,18 @@
+
+
+
+file1 = input("Enter The file Name:")
+if(file1):
+    file2 = input("Enter the Name of the file to be swapped ;} :")
+def swapFileData():
+    with open(file1,'r') as a:
+        data_a = a.read()
+    with open (file2,'r') as b:
+        data_b = b.read()
+    with open(file1,'w') as a:
+        a.write(data_b)
+    with open(file2,'w') as b:
+        b.write(data_a)
+
+
+swapFileData()
